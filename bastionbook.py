215e7b1f26f6a24a4853b1e9310d89ad3d35dbbd
Umapoornima <111286960+Umapoornima@users.noreply.github.com>
2024-04-18 10:45:41 +0530

    Update bastionbook.py

--- bastionbook.py
+++ bastionbook.py
@@ -13,31 +13,17 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World Durag and Uma!!</p>"
+    return "<p>Hello, World!@@!</p>"
 
 #for passing inputs through extensions
 #the decorator argument and the function argument
 # have to match.
 @app.route("/<name>")
 def hello_name(name):
-    return f"<h1>Hello, {escape(name)}!</h1>"
+    return f"<h1>Hello, {escape(name)}!</h2>"
 
 @app.route("/register", methods=('GET','POST'))
 def register():
-
-    #if request is GET
-    # url = 'https://8vciapa5d8.execute-api.us-west-2.amazonaws.com/test/dynamodbmanager'
-    url = 'http://backend-endpoint/'
-
-    get_all_data = {
-    'operation': 'getall'
-    }
-    
-    response_data = requests.post(
-        url,
-        json=get_all_data,
-        headers={'Accept': 'application/json'}
-    )
     if request.method == 'POST':
         try:
             inst_name = request.form['instance_name']
@@ -56,6 +42,7 @@ def register():
             print((inst_name, user, ipaddr, pvt_key))
 
             try:
+                url = 'https://8vciapa5d8.execute-api.us-west-2.amazonaws.com/test/dynamodbmanager'
                 new_entry = {
                     'operation': str(operation),
                     'payload': {
@@ -67,27 +54,23 @@ def register():
                         }
                     }
                 }
-
                 response = requests.post(
                     url, 
-                    json=new_entry,
-                    headers={'Accept': 'application/json'}
+                    data=json.dumps(new_entry)
                     )
-                # print(response.json())
-                assert response.json()["ResponseMetadata"]["HTTPStatusCode"] == 200
+                assert response.content.decode() == 'null'
                 flash('entry successful')
-                return redirect(url_for('register'))
+                return render_template('register.html', bad_entry=False)
             except:
                 flash('entry unsuccessful, internal error.')
-                return render_template('register.html', bad_entry=True, instance_data=response_data.json())
+                return render_template('register.html', bad_entry=True)
         
         except:
             flash('entry unsuccessful, try again.')
-            return render_template('register.html', bad_entry=True, instance_data=response_data.json())
-         
+            return render_template('register.html', bad_entry=True)
+                
             
-    return render_template('register.html', instance_data=response_data.json())
-
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
